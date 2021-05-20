b30eeeedb159acd789fcf478726acb95b2ba0402
Akshay Mishra <51031884+Akshaymishra211@users.noreply.github.com>
2021-05-20 15:02:44 +0530

    Update Xor Equality.java

--- May Challenge 2021/Xor Equality.java
+++ May Challenge 2021/Xor Equality.java
@@ -1,1 +1,90 @@
-//coming soon....
+import java.io.*;
+import java.util.*;
+
+class A {
+	//lexographically best moves...
+	public static int dx[] = { 1, 0, 0, -1 };
+	public static int dy[] = { 0, -1, 1, 0 };
+	public static int mod=1000000007;
+	//(x^y)%mod;
+	public static long power(long x,long y){
+		long res = 1;
+		x = x % mod;
+		
+		while (y > 0){
+		  if ((y & 1) > 0)
+		      res = (res * x) % mod;
+		  y = y >> 1;
+		  x = (x * x) % mod;
+		}
+		return res%mod;
+	}
+	public static void main(String[] args) throws FileNotFoundException {
+		FastScanner fs = new FastScanner();
+		FastOutput fo = new FastOutput(System.out);
+		long initial_time = System.currentTimeMillis();
+		int testcases = fs.nextInt();
+		for (int tt = 0; tt < testcases; tt++) {
+			//main code
+			int n=fs.nextInt();
+			fo.println(power(2, n-1));
+		}
+		//fo.time(initial_time);
+		fo.close();
+	}
+
+	static class FastScanner {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastScanner() throws FileNotFoundException {
+			//if (System.getProperty("ONLINE_JUDGE") == null) {
+				//Read from the File...
+			//	File file = new File("src\\input");
+			//	br = new BufferedReader(new FileReader(file));
+			//} else {
+				//Read from the System...
+				br = new BufferedReader(new InputStreamReader(System.in));
+			//}
+			st = new StringTokenizer("");
+		}
+
+		String next() {
+			while (!st.hasMoreTokens())
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		int[] readArray(int n) {
+			int[] a = new int[n];
+			for (int i = 0; i < n; i++)
+				a[i] = nextInt();
+			return a;
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+	}
+
+	static class FastOutput extends PrintWriter {
+
+		public FastOutput(PrintStream out) {
+			super(out);
+		}
+
+		void time(long init) {
+			if (System.getProperty("ONLINE_JUDGE") == null) {
+				this.println(System.currentTimeMillis() - init + "ms");
+			}
+		}
+	}
+}
